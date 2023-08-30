8c9494dffdb1eb1eb8a3360dd43892d29df03eb7
David <81187353+nob322@users.noreply.github.com>
2023-08-29 22:07:15 -0300

    David
    Semana 3

---
+++ Python/Semana3/tipo_float.py
@@ -0,0 +1,9 @@
+# Profundizando en el tipo float
+
+a = 3.0
+
+# Constructor de tipo float -> puede recibir int y str
+a = float(10) # Le pasamos un tipo entero (int)
+a = float('10')
+print(f'a: {a:.2f}')
+
