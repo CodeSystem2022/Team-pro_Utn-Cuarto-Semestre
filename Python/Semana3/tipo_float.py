de80c4ae538d9ec3a144b0ee16f19dc3204eb081
David <81187353+nob322@users.noreply.github.com>
2023-08-29 23:02:49 -0300

    David
    Semana 3

--- Python/Semana3/tipo_float.py
+++ Python/Semana3/tipo_float.py
@@ -1,11 +1,11 @@
+# Profundizando en el tipo float
 
+a = 3.0
 
-
-
-
-
-
-
+# Constructor de tipo float -> puede recibir int y str
+a = float(10) # Le pasamos un tipo entero (int)
+a = float('10')
+print(f'a: {a:.2f}')
 
 # Notación exponencial (valores positivos o negativos)
 a = 3e5
@@ -19,4 +19,3 @@ print(f'a: {a:.5f}')
 a = 4.0 + 5
 print(a)
 print(type(a))
->>>>>>> Stashed changes
