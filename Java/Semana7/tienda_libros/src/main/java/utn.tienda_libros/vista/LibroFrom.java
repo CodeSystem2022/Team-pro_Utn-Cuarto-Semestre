928cfdbdb5bea57e8c7544914a705e1c681cdfeb
Sandra Ferreyra <sandraferreyrau2@gmail.com>
2023-09-30 00:22:35 -0300

    Sandra
    Semana7!

---
+++ Java/Semana7/tienda_libros/src/main/java/utn.tienda_libros/vista/LibroFrom.java
@@ -0,0 +1,20 @@
+package utn.tienda_libros.vista;
+
+import javax.swing.*;
+
+public class LibroFrom extends JFrame {
+    LibroServicio libroServicio;
+    
+    @Autowired
+    public LibroFrom(LibroServicio libroServicio){
+       this.libroServicio = libroServicio;
+       iniciarForma();
+    }
+    
+    private void iniciarForma(){
+        setContentPane(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(900, 700);
+    }
+  }
