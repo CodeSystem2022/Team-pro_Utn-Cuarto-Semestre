439dc8fe13f258b163f2788699462a8f79a5655f
Fernando Gregoratti <fernandogregoratti@gmail.com>
2023-11-04 20:45:21 -0300

    Fernando
    Presente semana 12

---
+++ Java/Semana12/tienda_libros/repository/service/TodoAppApplication.java
@@ -0,0 +1,14 @@
+package ar.com.utnfrsr.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 
+public class TodoAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TodoAppApplication.class), args);
+        System.out.println("Hola desde SpringBoot");
+    }
+}
